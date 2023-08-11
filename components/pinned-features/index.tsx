676c4b69bbed13f751cb364042efb0c6213718ea
armagansenol <armagansnl@gmail.com>
2023-08-11 10:49:00 +0300

    update texts animations

--- components/pinned-features/index.tsx
+++ components/pinned-features/index.tsx
@@ -3,6 +3,8 @@ import s from './pinned-features.module.scss'
 
 import cn from 'clsx'
 import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/dist/ScrollTrigger'
+
 import { useMedia } from 'react-use'
 
 // const mediaSrc =
@@ -12,7 +14,6 @@ const mediaSrc = '/video/pinned.mp4'
 
 const PinnedFeatures = () => {
   const ref = useRef(null)
-  const q = gsap.utils.selector(ref)
   const videoRef = useRef<HTMLVideoElement>(null)
   const isMobile = useMedia('(max-width: 800px)', true)
 
@@ -27,10 +28,12 @@ const PinnedFeatures = () => {
   )
 
   useEffect(() => {
+    const q = gsap.utils.selector(ref)
+
     function updateVideo(val: number) {
       if (videoRef.current) {
         const mapped = parseFloat(
-          gsap.utils.mapRange(0, 1, 0, 22, val).toFixed(3)
+          gsap.utils.mapRange(0, 1, 0, 22, val).toString()
         )
         videoRef.current.currentTime = mapped
       }
@@ -39,117 +42,103 @@ const PinnedFeatures = () => {
     const ctx = gsap.context(() => {
       if (isMobile) return
 
-      const scroll = q('[data-scroll]')
-      const boxes = q('.box')
+      const scrollContainer = q('[data-animation-scroll-container]')
+      const items = q('[data-animation-scroll-item]')
 
-      const tl = gsap.timeline().to(
-        scroll,
-        {
-          y: `-${window.innerHeight * 3}px`,
-          ease: 'none',
-        },
-        's'
-      )
+      const scrollLength =
+        items[0].getBoundingClientRect().height * (items.length - 1)
+      const multiplyBy = 2
 
-      const containerAnim = gsap.to(scroll, {
-        y: `-${window.innerHeight * 3}px`,
+      gsap.to(scrollContainer, {
         ease: 'none',
+        y: `-${scrollLength}px`,
         scrollTrigger: {
-          end: '5000px top',
+          end: `${scrollLength * multiplyBy}px top`,
           id: 'pinned-features',
           // markers: true,
-          onUpdate: (self) => {
-            //   console.log('progress', self.progress)
-            updateVideo(self.progress)
-          },
           pin: true,
           pinSpacing: true,
-          scrub: true,
+          scrub: 1,
           trigger: ref.current,
+          onUpdate: (self) => {
+            updateVideo(self.progress)
+          },
         },
       })
 
-      // const containerAnim = ScrollTrigger.create({
-      //   id: 'pinned-features',
-      //   animation: tl,
-      //   scrub: true,
-      //   trigger: ref.current,
-      //   // markers: true,
-      //   pin: true,
-      //   pinSpacing: true,
-      //   // end: '5000px top',
-      //   onUpdate: (self) => {
-      //     //   console.log('progress', self.progress)
-      //     updateVideo(self.progress)
-      //   },
-      // })
+      const tl = gsap
+        .timeline({ paused: true })
+        .to(items[0], { opacity: 1 })
+        .to(items[0], { opacity: 0.3 })
 
-      gsap.set(boxes, {
-        opacity: 0.3,
-      })
+        .to(items[1], { opacity: 0.3 })
+        .to(items[1], { opacity: 1 })
+        .to(items[1], { opacity: 0.3 })
 
-      boxes.map((box, i) => {
-        // red section
-        gsap.to(box, {
-          opacity: 1,
-          scrollTrigger: {
-            trigger: box,
-            toggleClass: 'active',
-            start: 'center bottom',
-            end: 'center top',
-            id: i.toString(),
-            // markers: true,
-          },
-        })
+        .to(items[2], { opacity: 0.3 })
+        .to(items[2], { opacity: 1 })
+
+      ScrollTrigger.create({
+        scrub: 1,
+        animation: tl,
+        trigger: ref.current,
+        end: `${scrollLength * multiplyBy}px top`,
+        // markers: true,
       })
     })
 
     return () => ctx.revert()
-  }, [q])
+  }, [isMobile])
 
   return (
     <div className={s.pinnedFeatures} ref={ref}>
-      <div className={s.features}>
-        <div className={s.transformC} data-scroll>
-          <div className={cn(s.box, 'box')}>
-            <h3>All-in-one Software</h3>
-            <p>
-              Layers defines the employee experience by placing people at the
-              heart of all people operations systems and processes.
-            </p>
-            <p>
-              By leveraging self-service functionality and seamless approvals,
-              Layers ensures that employees can effortlessly access the
-              resources they need without unnecessary hurdles.
-            </p>
+      <div className="hidden-overflow">
+        <div className={s.transformC} data-animation-scroll-container>
+          <div className={s.item} data-animation-scroll-item>
+            <div className={s.text}>
+              <h3>All-in-one Software</h3>
+              <p>
+                Layers defines the employee experience by placing people at the
+                heart of all people operations systems and processes.
+              </p>
+              <p>
+                By leveraging self-service functionality and seamless approvals,
+                Layers ensures that employees can effortlessly access the
+                resources they need without unnecessary hurdles.
+              </p>
+            </div>
           </div>
-          <div className={cn(s.box, 'box')}>
-            <h3>All-in-one Software</h3>
-            <p>
-              Layers defines the employee experience by placing people at the
-              heart of all people operations systems and processes.
-            </p>
-            <p>
-              By leveraging self-service functionality and seamless approvals,
-              Layers ensures that employees can effortlessly access the
-              resources they need without unnecessary hurdles.
-            </p>
+          <div className={s.item} data-animation-scroll-item>
+            <div className={s.text}>
+              <h3>All-in-one Software</h3>
+              <p>
+                Layers defines the employee experience by placing people at the
+                heart of all people operations systems and processes.
+              </p>
+              <p>
+                By leveraging self-service functionality and seamless approvals,
+                Layers ensures that employees can effortlessly access the
+                resources they need without unnecessary hurdles.
+              </p>
+            </div>
           </div>
-          <div className={cn(s.box, 'box')}>
-            <h3>All-in-one Software</h3>
-            <p>
-              Layers defines the employee experience by placing people at the
-              heart of all people operations systems and processes.
-            </p>
-            <p>
-              By leveraging self-service functionality and seamless approvals,
-              Layers ensures that employees can effortlessly access the
-              resources they need without unnecessary hurdles.
-            </p>
+          <div className={s.item} data-animation-scroll-item>
+            <div className={s.text}>
+              <h3>All-in-one Software</h3>
+              <p>
+                Layers defines the employee experience by placing people at the
+                heart of all people operations systems and processes.
+              </p>
+              <p>
+                By leveraging self-service functionality and seamless approvals,
+                Layers ensures that employees can effortlessly access the
+                resources they need without unnecessary hurdles.
+              </p>
+            </div>
           </div>
         </div>
       </div>
-      <div className={cn(s.media, 'flex-center')}>
+      <div className="flex-center">
         <div className={s.videoC}>{media}</div>
       </div>
     </div>
