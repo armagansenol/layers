af185627eb04c0b0ed703c74adf968325b365939
armagansenol <armagansnl@gmail.com>
2023-10-11 15:38:53 +0300

    update animations

--- components/navbar/desktop/index.tsx
+++ components/navbar/desktop/index.tsx
@@ -1,47 +1,97 @@
-import { useRef } from 'react'
+import { useLayoutEffect, useRef } from 'react'
 import s from './navbar-desktop.module.scss'
 
 import cn from 'clsx'
-import { AnimatePresence, motion } from 'framer-motion'
-import Link from 'next/link'
+import gsap from 'gsap'
 import { useTranslation } from 'next-i18next'
+import Link from 'next/link'
 
-import IconArrowDropdown from '@/components/icons/icon-arrow-dropdown'
 import CustomImage from '@/components/custom-image'
+import IconArrowDropdown from '@/components/icons/icon-arrow-dropdown'
 import { Locales, MainRoute, routes } from '@/global'
 import { useMenuStore } from '@/lib/store/menu'
-import { customEase1 } from '@/utils'
 
 export function NavbarDesktop() {
-  const { currentRoute, isOpen, setCurrentRoute, setIsOpen } = useMenuStore()
-  const menuRef = useRef<HTMLElement>(null)
+  const ref = useRef<HTMLElement>(null)
+  const q = gsap.utils.selector(ref)
   const { i18n, t } = useTranslation('common')
+  const { currentRoute, isOpen, setCurrentRoute, setIsOpen } = useMenuStore()
+  const currentLang = i18n.language as Locales
+  const animation = useRef<gsap.core.Tween>()
+
+  useLayoutEffect(() => {
+    const ctx = gsap.context(() => {
+      animation.current = gsap.from(q('.menu'), {
+        opacity: 0,
+        scale: 0.5,
+        yPercent: -10,
+        pointerEvents: 'none',
+        duration: 0.2,
+        onComplete: () => {
+          gsap.to(q('.menu'), {
+            pointerEvents: 'auto',
+          })
+        },
+      })
+    })
+
+    return () => ctx.revert()
+  }, [])
 
   function handleMenu(type: MainRoute) {
+    // closeMenu()
+
+    // gsap.to(q('.links'), {
+    //   opacity: 0,
+    //   duration: 0.2,
+    //   onComplete: () => {
+    //     if (type) {
+    //       setCurrentRoute(type)
+    //     }
+
+    //     gsap.to(q('.links'), {
+    //       opacity: 1,
+    //       delay: 0.2,
+    //       duration: 0.2,
+    //     })
+    //   },
+    // })
+
     if (type) {
       setCurrentRoute(type)
     }
 
+    openMenu()
+  }
+
+  function openMenu() {
     setIsOpen(true)
   }
 
-  function handleClose() {
+  function closeMenu() {
     setIsOpen(false)
   }
 
+  useLayoutEffect(() => {
+    if (!animation.current) return
+
+    if (isOpen) {
+      animation.current.play()
+    } else {
+      animation.current.reverse()
+    }
+  }, [isOpen])
+
   return (
     <nav
-      ref={menuRef}
       className={cn(s.navigation, s.fixed, [
-        s[
-          currentRoute
-            ? routes[i18n.language as Locales][currentRoute].type
-            : 'null'
-        ],
+        s[currentRoute ? routes[currentLang][currentRoute].type : 'null'],
       ])}
-      onMouseLeave={handleClose}
+      ref={ref}
+      onMouseLeave={closeMenu}
     >
-      {Object.values(routes[i18n.language as Locales]).map((value, i) => {
+      {/* navbar items */}
+      {Object.values(routes[currentLang]).map((value, i) => {
         return (
           <div
             className={cn(s.navItemC, 'cursor-pointer', [s[value.type]], {
@@ -57,98 +107,59 @@ export function NavbarDesktop() {
           </div>
         )
       })}
+
       <Link
-        href="/demo-request"
         className={cn(s.navItemC, s.requestADemo, 'cursor-pointer')}
-        onMouseEnter={handleClose}
+        href="/demo-request"
+        onMouseEnter={closeMenu}
       >
         <p>{t('header.requestADemo')}</p>
       </Link>
-      <AnimatePresence mode="popLayout">
-        {isOpen && (
-          <motion.div className={s.menuC}>
-            <motion.div
-              className={cn(s.menu, 'flex-center')}
-              key={`${currentRoute}-menu`}
-              initial="closed"
-              animate="open"
-              exit="closed"
-              variants={{
-                open: {
-                  opacity: 1,
-                  scale: 1,
-                  y: 0,
-                  transition: { duration: 0.5, ease: customEase1 },
-                },
-                closed: {
-                  opacity: 0,
-                  scale: 0.75,
-                  y: -50,
-                  transition: { duration: 0.2, ease: customEase1 },
-                },
-              }}
-            >
-              <AnimatePresence>
-                {currentRoute && (
-                  <motion.div
-                    className={cn(s.links)}
-                    animate="open"
-                    exit="closed"
-                    initial="closed"
-                    key={`${currentRoute}-links`}
-                    variants={{
-                      open: {
-                        opacity: 1,
-                        transition: { duration: 0.1, ease: 'easeInOut' },
-                      },
-                      closed: {
-                        opacity: 0,
-                        transition: { duration: 0.1, ease: 'easeInOut' },
-                      },
-                    }}
-                  >
-                    {routes[i18n.language as Locales][currentRoute].children &&
-                      Object.values(
-                        routes[i18n.language as Locales][currentRoute].children
-                      ).map((item, i) => {
-                        return (
-                          <Link
-                            className={cn(s.menuItem, 'cursor-pointer')}
-                            href={`/${
-                              routes[i18n.language as Locales][currentRoute]
-                                .path
-                                ? routes[i18n.language as Locales][currentRoute]
-                                    .path + '/'
-                                : ''
-                            }${item.path}`}
-                            key={i}
-                            onClick={handleClose}
-                          >
-                            {currentRoute !== MainRoute.resources && (
-                              <div className={cn(s.iconC, 'flex-center')}>
-                                <CustomImage
-                                  alt="Feature Icons"
-                                  height={150}
-                                  src={`/img/detail/${item.path}/menu-icon.png`}
-                                  style={{ objectFit: 'contain' }}
-                                  width={150}
-                                />
-                              </div>
-                            )}
-                            <div className={s.text}>
-                              {item.ui && <h5>{item.ui}</h5>}
-                              {item.desc && <p>{item.desc}</p>}
-                            </div>
-                          </Link>
-                        )
-                      })}
-                  </motion.div>
-                )}
-              </AnimatePresence>
-            </motion.div>
-          </motion.div>
-        )}
-      </AnimatePresence>
+
+      {/* menu */}
+
+      <div className={cn(s.menuC, 'menuC')}>
+        <div
+          className={cn(s.menu, 'flex-center', 'menu')}
+          onMouseLeave={closeMenu}
+        >
+          <div className={cn(s.links, 'links')}>
+            {currentRoute &&
+              Object.values(routes[currentLang][currentRoute].children).map(
+                (item, i) => {
+                  const currentPath = routes[currentLang][currentRoute].path
+
+                  return (
+                    <Link
+                      className={cn(s.menuItem, 'cursor-pointer')}
+                      href={`/${currentPath ? currentPath + '/' : ''}${
+                        item.path
+                      }`}
+                      key={i}
+                      onClick={closeMenu}
+                    >
+                      {currentRoute !== MainRoute.resources && (
+                        <div className={cn(s.iconC, 'flex-center')}>
+                          <CustomImage
+                            alt="Route Icon"
+                            height={150}
+                            src={`/img/detail/${item.path}/menu-icon.png`}
+                            style={{ objectFit: 'contain' }}
+                            width={150}
+                          />
+                        </div>
+                      )}
+                      <div className={s.text}>
+                        {item.ui && <h5>{item.ui}</h5>}
+                        {item.desc && <p>{item.desc}</p>}
+                      </div>
+                    </Link>
+                  )
+                }
+              )}
+          </div>
+        </div>
+      </div>
     </nav>
   )
 }
